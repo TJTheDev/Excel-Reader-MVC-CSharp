997e498715aaa57cfa369baa1c7e0bb3c6d1cf6e
TJ <tarrylon1@hotmail.com>
2023-06-06 12:39:26 -0500

    I made Changes to the files and fixed the Error handling for unsupported file types.

--- ExcelMVC/Controllers/HomeController.cs
+++ ExcelMVC/Controllers/HomeController.cs
@@ -33,8 +33,17 @@ namespace ExcelMVC.Controllers
                     file.CopyTo(stream);
                 }
 
-                var dt = ReadExcelToDataTable(filePath);
-                return View(dt);
+                try
+                {
+                    var dt = ReadExcelToDataTable(filePath);
+                    return View(dt);
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.ErrorMessage = "Error occurred while processing the Excel file: " + ex.Message;
+                    System.IO.File.Delete(filePath); // If it's not an ExcelSheet Delete the uploaded file
+                    return View("Index");
+                }
             }
 
             return RedirectToAction("Index");
@@ -42,31 +51,40 @@ namespace ExcelMVC.Controllers
 
         private DataTable ReadExcelToDataTable(string filePath)
         {
-            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            try
             {
-                ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-                var workbook = package.Workbook;
-                var worksheet = workbook.Worksheets[0];
-                var dt = new DataTable();
-
-                foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns])
+                using (var package = new ExcelPackage(new FileInfo(filePath)))
                 {
-                    dt.Columns.Add(firstRowCell.Text);
-                }
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                    var workbook = package.Workbook;
+                    var worksheet = workbook.Worksheets[0];
+                    var dt = new DataTable();
 
-                for (var rowNum = 2; rowNum <= worksheet.Dimension.Rows; rowNum++)
-                {
-                    var worksheetRow = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.Columns];
-                    var row = dt.Rows.Add();
-                    foreach (var cell in worksheetRow)
+                    foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.Columns])
                     {
-                        row[cell.Start.Column - 1] = cell.Text;
+                        dt.Columns.Add(firstRowCell.Text);
                     }
-                }
 
-                return dt;
+                    for (var rowNum = 2; rowNum <= worksheet.Dimension.Rows; rowNum++)
+                    {
+                        var worksheetRow = worksheet.Cells[rowNum, 1, rowNum, worksheet.Dimension.Columns];
+                        var row = dt.Rows.Add();
+                        foreach (var cell in worksheetRow)
+                        {
+                            row[cell.Start.Column - 1] = cell.Text;
+                        }
+                    }
+
+                    return dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle the EPPlus exception (e.g., log, display an error message, etc.)
+                Console.WriteLine("Invalid Excel file: " + ex.Message);
+                throw new ApplicationException("Error occurred while reading Excel file: " + ex.Message);
             }
         }
     }
-
+ 
 }
